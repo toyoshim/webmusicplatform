fb04f7754a40512584ea982fe978d69338edeb53
Takashi Toyoshima <toyoshim@gmail.com>
2014-10-22 01:57:17 +0900

    Add support files to deploy on heroku

---
+++ web.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+app.use(logfmt.requestLogger());
+app.use(express.static(__dirname));
+var port = Number(process.env.PORT) || 8080;
+app.listen(port, function () {
+  console.log("Listening on " + port);
+});
